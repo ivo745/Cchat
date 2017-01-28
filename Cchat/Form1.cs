d7ae037c5299c259c16ba2ffc1498d37fd84980c
Ivo745 <ivo-lemmen@outlook.com>
2017-01-28 12:50:15 +0100

    Add project files.

---
+++ Cchat/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Cchat
+{
+    public partial class Form1 : Form
+    {
+        private TcpClient client;
+        public StreamReader STR;
+        public StreamWriter STW;
+        public string receive;
+        public String text_to_send;
+
+        // Startup application
+        public Form1()
+        {
+            InitializeComponent();
+            IPAddress[] localIP = Dns.GetHostAddresses(Dns.GetHostName());
+            foreach(IPAddress adress in localIP)
+            {
+                if (adress.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    textBox3.Text = adress.ToString();
+                }
+            }
+        }
+
+        // Start server
+        private void button2_Click(object sender, EventArgs e)
+        {
+            TcpListener server = null;
+
+            try
+            {
+                server = new TcpListener(IPAddress.Any, int.Parse(textBox4.Text));
+                server.Start();
+
+                while (true)
+                {
+                    client = server.AcceptTcpClient();
+
+                    STR = new StreamReader(client.GetStream());
+                    STW = new StreamWriter(client.GetStream());
+                    STW.AutoFlush = true;
+
+                    backgroundWorker1.RunWorkerAsync();
+                    backgroundWorker2.WorkerSupportsCancellation = true;
+                }
+            }
+            catch (Exception x)
+            {
+                MessageBox.Show(x.Message.ToString());
+            }
+        }
+
+        private void createLog(string path)
+        {
+            path = (System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)) + "Cchat";
+            Directory.CreateDirectory(path);
+        }
+
+        // Receive data
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while(client.Connected)
+            {
+                try
+                {
+                    receive = STR.ReadLine();
+                    this.textBox2.Invoke(new MethodInvoker(delegate () { textBox2.AppendText("You: " + receive + "\n"); }));
+                    receive = "";
+                    createLog("");
+                }
+                catch(Exception x)
+                {
+                    MessageBox.Show(x.Message.ToString());
+                }
+            }
+        }
+
+        // Send data
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+        {
+            if (client.Connected)
+            {
+                STW.WriteLine(text_to_send);
+                this.textBox2.Invoke(new MethodInvoker(delegate () { textBox2.AppendText("me: " + text_to_send + "\n"); }));
+            }
+            else
+            {
+                MessageBox.Show("Send failed!");
+            }
+            backgroundWorker2.CancelAsync();
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                client = new TcpClient();
+                IPEndPoint IP_End = new IPEndPoint(IPAddress.Parse(textBox5.Text), int.Parse(textBox6.Text));
+                client.Connect(IP_End);
+                if (client.Connected)
+                {
+                    textBox2.AppendText("Connected to server" + "\n");
+                    STR = new StreamReader(client.GetStream());
+                    STW = new StreamWriter(client.GetStream());
+                    STW.AutoFlush = true;
+
+                    backgroundWorker1.RunWorkerAsync();
+                    backgroundWorker2.WorkerSupportsCancellation = true;
+                }
+            }
+            catch(Exception x)
+            {
+                MessageBox.Show(x.Message.ToString());
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                text_to_send = textBox1.Text;
+                backgroundWorker2.RunWorkerAsync();
+            }
+            textBox1.Text = "";
+        }
+    }
+}
---
+++ Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CchatLog
+{
+    public class CchatLog
+    {
+        private void createLog(string path)
+        {
+            path = (System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)) + "Cchat";
+            Directory.CreateDirectory(path);
+        }
+    }
+}
