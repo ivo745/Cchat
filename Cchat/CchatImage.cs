491d18d5aa5fe1e8547f6eaf8c95dd8d96534b08
Ivo745 <ivo-lemmen@outlook.com>
2017-03-26 11:15:50 +0200

    Remove unused code

--- Cchat/CchatImage.cs
+++ Cchat/CchatImage.cs
@@ -20,34 +20,10 @@ namespace Cchat
 
         private static ImageFormat GetImageFormat(byte[] value)
         {
-            var bmp = Encoding.ASCII.GetBytes("BM");     // BMP
-            var gif = Encoding.ASCII.GetBytes("GIF");    // GIF
-            var png = new byte[] { 137, 80, 78, 71 };    // PNG
-            var tiff = new byte[] { 73, 73, 42 };         // TIFF
-            var tiff2 = new byte[] { 77, 77, 42 };         // TIFF
-            var jpeg = new byte[] { 255, 216, 255, 224 }; // jpeg
-            var jpeg2 = new byte[] { 255, 216, 255, 225 }; // jpeg canon
-
-            if (bmp.SequenceEqual(value.Take(bmp.Length)))
-                return ImageFormat.Bmp;
-
-            if (gif.SequenceEqual(value.Take(gif.Length)))
-                return ImageFormat.Gif;
+            var png = new byte[] { 137, 80, 78, 71 };
 
             if (png.SequenceEqual(value.Take(png.Length)))
                 return ImageFormat.Png;
-
-            if (tiff.SequenceEqual(value.Take(tiff.Length)))
-                return ImageFormat.Tiff;
-
-            if (tiff2.SequenceEqual(value.Take(tiff2.Length)))
-                return ImageFormat.Tiff;
-
-            if (jpeg.SequenceEqual(value.Take(jpeg.Length)))
-                return ImageFormat.Jpeg;
-
-            if (jpeg2.SequenceEqual(value.Take(jpeg2.Length)))
-                return ImageFormat.Jpeg;
 
             return ImageFormat.Unknown;
         }
